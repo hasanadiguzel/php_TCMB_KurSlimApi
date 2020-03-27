74b6aedf550f71dca3cb459c43ccc415a6af733b
Hasan Adıgüzel <57960003+hasanadiguzel@users.noreply.github.com>
2020-03-27 21:51:33 +0300

    Update courses.php

--- slimapp/src/routes/courses.php
+++ slimapp/src/routes/courses.php
@@ -9,7 +9,7 @@ $app->add(function ($req, $res, $next) {
     return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 });
 
 // TCMB guncel kuru getir...
